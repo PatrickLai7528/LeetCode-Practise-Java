5f339331282faa5469110cbe0b65f5f591948876
161250051 <m13236537528_1@163.com>
2018-07-13 14:31:04 +0800

    完成451. Sort Characters By Frequency

---
+++ Exercise/src/Ex451SortCharactersByFrequency/Solution.java
@@ -0,0 +1,77 @@
+package Ex451SortCharactersByFrequency;
+
+import java.util.*;
+
+/**
+ * Given a string, sort it in decreasing order based on the frequency of characters.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * "tree"
+ * <p>
+ * Output:
+ * "eert"
+ * <p>
+ * Explanation:
+ * 'e' appears twice while 'r' and 't' both appear once.
+ * So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+ * Example 2:
+ * <p>
+ * Input:
+ * "cccaaa"
+ * <p>
+ * Output:
+ * "cccaaa"
+ * <p>
+ * Explanation:
+ * Both 'c' and 'a' appear three times, so "aaaccc" is also a valid answer.
+ * Note that "cacaca" is incorrect, as the same characters must be together.
+ * Example 3:
+ * <p>
+ * Input:
+ * "Aabb"
+ * <p>
+ * Output:
+ * "bbAa"
+ * <p>
+ * Explanation:
+ * "bbaA" is also a valid answer, but "Aabb" is incorrect.
+ * Note that 'A' and 'a' are treated as two different characters.
+ */
+
+public class Solution {
+    public String frequencySort(String s) {
+        Comparator<Map.Entry<Character, Integer>> comparator = new Comparator<Map.Entry<Character, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Character, Integer> o1, Map.Entry<Character, Integer> o2) {
+                return o2.getValue() - o1.getValue();
+            }
+        };
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
+            } else {
+                map.put(s.charAt(i), 1);
+            }
+        }
+        List<Map.Entry<Character, Integer>> list = new ArrayList<Map.Entry<Character, Integer>>(map.entrySet());
+        Collections.sort(list, comparator);
+        System.out.println(list);
+        String result = "";
+        for (Map.Entry<Character, Integer> entry : list) {
+            String key = String.valueOf(entry.getKey());
+            int time = entry.getValue();
+            for (int i = 0; i < time; i++) {
+                result += key;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.frequencySort("aabbbbbbbccccddddd"));
+    }
+}
